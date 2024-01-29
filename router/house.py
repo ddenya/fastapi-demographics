895103c5ef7b2eb837f999ec4b5e781da2c2749c
Okan Güneş <okngns1@outlook.com>
2024-01-29 16:36:51 +0100

    add2 router/house.py

--- router/house.py
+++ router/house.py
@@ -3,7 +3,7 @@ from db.schemas import HouseDisplay, HouseBase
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.database import get_db
-from db import db_article
+from db import db_house
 
 router = APIRouter(
     prefix='/house',
