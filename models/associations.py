0b0fa7ba3862ec9a29b78a067819c3ff63d29ebc
chechetov <dr.dan79@gmail.com>
2024-01-30 18:16:40 +0100

    add id models/associations.py

--- models/associations.py
+++ models/associations.py
@@ -4,6 +4,7 @@ from sqlalchemy import Table, Column, Integer, ForeignKey
 people_houses = Table(
     'people_houses',
     Base.metadata,
+    Column('id', Integer, primary_key=True),
     Column('person_id', Integer, ForeignKey('person.id')),
     Column('house_id', Integer, ForeignKey('houses.id'))
 )
