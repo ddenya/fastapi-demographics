c4acc83053021bc9f0ba43df63303ceea528243b
chechetov <dr.dan79@gmail.com>
2024-01-31 15:33:09 +0100

    add add_houses_by_ids to models/person.py

--- models/person.py
+++ models/person.py
@@ -4,6 +4,8 @@ from sqlalchemy.sql.sqltypes import Integer, String, Boolean
 from sqlalchemy import Column
 from sqlalchemy.orm import relationship
 from models.associations import people_houses
+# TODO refactor later?
+from sqlalchemy.orm import Session
 
 class DbPerson(Base):
   __tablename__ = 'person'
@@ -14,3 +16,9 @@ class DbPerson(Base):
   email = Column(String)
   nation = Column(String)
   houses = relationship('DbHouse', secondary='people_houses', back_populates='owners')
+
+# TODO refactor later?
+  def add_houses_by_ids(self, houses_ids: list[int], session: Session):
+    # Avoiding cirtular import
+    from models.house import DbHouse
+    self.houses.extend(session.query(DbHouse).filter(DbHouse.id.in_(houses_ids)).all())
