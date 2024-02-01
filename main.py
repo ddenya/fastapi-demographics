e589357a7b36053a05376f62da907d4e5f7a9983
chechetov <dr.dan79@gmail.com>
2024-02-01 14:31:24 +0100

    add CORS

--- main.py
+++ main.py
@@ -2,12 +2,25 @@ from fastapi import FastAPI
 from router import user, house, person, car
 from models import Base
 from db.db_connector import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(user.router)
 app.include_router(house.router)
 app.include_router(car.router)
 app.include_router(person.router)
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],  # You can specify specific HTTP methods if needed
+    allow_headers=["*"],  # You can specify specific HTTP headers if needed
+)
 
 
 # If database exists - it does not create it again
