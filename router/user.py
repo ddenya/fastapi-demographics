8511843995b79c9118e061406a4459e0996f246c
Okan Güneş <okngns1@outlook.com>
2024-02-14 12:45:04 +0100

    add user authentication for router/user.py

--- router/user.py
+++ router/user.py
@@ -5,7 +5,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.db_connector import get_db
 from db_crud import user as db_user
-from auth.oauth2 import check_user_types
+from auth.oauth2 import check_user_operations_privileges, check_user_types, get_current_user
 
 router = APIRouter(
   prefix='/user',
@@ -22,9 +22,13 @@ def create_user(request: UserBase, db: Session=Depends(get_db)):
 def get_all_users(db: Session=Depends(get_db)):
   return db_user.get_all_users(db)
 
+
 # Read one user 
 @router.get('/{id}', response_model=UserDisplay, status_code=200, dependencies=[Depends(check_user_types(['admin', 'member', 'auditor']))])
-def get_user(id: int, db: Session=Depends(get_db)):
+def get_user(id: int, db: Session=Depends(get_db),  current_user: UserDisplay = Depends(get_current_user)):
+
+  check_user_operations_privileges(current_user, id) 
+
   ret = db_user.get_user(id,db)
   if ret is None:
     return JSONResponse(
@@ -33,16 +37,21 @@ def get_user(id: int, db: Session=Depends(get_db)):
     )
   return ret
 
+
 # Update
 @router.patch('/{id}', response_model=UserDisplay, status_code=200, dependencies=[Depends(check_user_types(['admin', 'member']))])
-def update_user(id: int, request: UserBase, db: Session=Depends(get_db)):
-  ret = db_user.update_user(id, request, db)
-  if ret is None:
-    return JSONResponse(
-        status_code=404,
-        content={"message": f'User with {id} not found'},
-    )
-  return ret
+def update_user(id: int, request: UserBase, db: Session=Depends(get_db),  current_user: UserDisplay = Depends(get_current_user)):
+  
+  check_user_operations_privileges(current_user, id) 
+ 
+  user = db_user.update_user(id, request, db)
+  if user is None:
+   return JSONResponse(
+      status_code=404,
+      content={"message": f'User with {id} not found'},
+  )
+  return user
+
 
 # Delete
 @router.delete('/{id}', status_code=204, dependencies=[Depends(check_user_types('admin'))])
